5a0549c2fa446b14f029648eecffe8bf8fa96593
with-key <rem9238@kakao.com>
2022-11-26 16:25:45 +0900

    add: stitches global style configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from "next/app";
+import { globalStyles } from "../styles/globalstyle";
 
 export default function App({ Component, pageProps }: AppProps) {
+  globalStyles();
   return <Component {...pageProps} />;
 }
